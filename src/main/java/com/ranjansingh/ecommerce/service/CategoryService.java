70a2c5135c12b2354d77f14e1b1e8d9aa7c9e69e
Ranjan Singh <ranjansingh233@gmail.com>
2023-11-24 18:14:49 +0530

    updated the functionality of delete and update of categories

--- src/main/java/com/ranjansingh/ecommerce/service/CategoryService.java
+++ src/main/java/com/ranjansingh/ecommerce/service/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -18,5 +19,12 @@ public class CategoryService {
     }
     public List<Category> getAllCategory(){
         return categoryRepo.findAll();
+    }
+    public void removeCategoryById(int id){
+        categoryRepo.deleteById(id);
+    }
+
+    public Optional<Category> getCategoryById(int id){
+        return categoryRepo.findById(id);
     }
 }
